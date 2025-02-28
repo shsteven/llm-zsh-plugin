734a63996e9e58f8c2cb70fee623720ee9bd270c
Eliya Stein <eliya.stein@gmail.com>
2025-02-28 00:16:09 -0500

    fix init path

--- llm.plugin.zsh
+++ llm.plugin.zsh
@@ -2,9 +2,8 @@
 # Provides completion support for the LLM CLI tool (https://llm.datasette.io/)
 
 # Add the completion script to the Zsh function path
-fpath+=${0:h}/completions
+fpath=(${0:h}/completions $fpath)
 
 # Initialize completion
 autoload -U compinit
 compinit
-
